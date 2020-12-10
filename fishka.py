4c35255e99359032df1b9a8475c98e06e0bce6bf
Dima <dim.taigo@gmail.com>
2020-12-10 17:07:02 +0600

    added dad and mom pages

--- fishka.py
+++ fishka.py
@@ -10,9 +10,19 @@ def home():
 def hello_bakha():
     return render_template("bakha.html")
 
+@app.route("/dad/")
+def hello_dad():
+    return render_template("dad.html")
+
+@app.route("/mom/")
+def hello_mom():
+    return render_template("mom.html")
+
+
 @app.route("/<name>/")
 def hello_bye(name):
     return f"Привет {name}, и проваливай!"
 
 if __name__ == "__main__":
-    app.run()
+    app.debug = True
+    app.run(host = "0.0.0.0")
